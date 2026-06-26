85efa7cd6629ad79f7624a2effc44092f969f583
Oh Fu Sheng <kelvinoh.fusheng.9@gmail.com>
2026-06-26 17:52:37 +0800

    Add guardian configuration file with initial settings

---
+++ guardian_config.py
@@ -0,0 +1,13 @@
+# guardian_config.py (laptop)
+
+DEVICE_NAME = "QuMinds-AIoT-TJX"
+# For classroom:
+# DEVICE_NAME = "Guardian-01"
+
+CHAR_UUID = "12345678-1234-5678-1234-56789abcdef1"
+
+WEB_PORT = 8000
+CSV_FILE = "guardian_training_windows.csv"
+
+# Each ML training row represents a 3-second behaviour window
+ML_WINDOW_SECONDS = 3.0
